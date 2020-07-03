12f174685f3daac032b882b725244578093e564e
albert <amyik100@gmail.com>
2020-07-03 22:11:29 +0900

    #6 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+// import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/stable';
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
+    // <Header></Header>
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" component={About}></Route>
     </HashRouter>
+    // <Footer></Footer>
   );
 }
 
